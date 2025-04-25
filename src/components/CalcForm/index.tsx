9f47bc9576e4c0f6e9b69b9e66cdb64b706ea179
henriquegs7 <henriquegs.contato@gmail.com>
2025-04-25 20:22:17 -0300

    ajust

--- src/components/CalcForm/index.tsx
+++ src/components/CalcForm/index.tsx
@@ -47,9 +47,9 @@ export const CalculatorForm = ({ onCalculate, onClear, sugestoesProjetos = [] }:
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
 
-    const valorRoloNum = Number.parseFloat(valorRolo)
-    const pesoRoloNum = Number.parseFloat(pesoRolo)
-    const pesoProjetoNum = Number.parseFloat(pesoProjeto)
+    const valorRoloNum = Number.parseFloat(valorRolo.replace(',', '.'))
+    const pesoRoloNum = Number.parseFloat(pesoRolo.replace(',', '.'))
+    const pesoProjetoNum = Number.parseFloat(pesoProjeto.replace(',', '.'))
 
     if (isNaN(valorRoloNum) || isNaN(pesoRoloNum) || isNaN(pesoProjetoNum) || pesoRoloNum === 0) {
       alert("Por favor, insira valores válidos")
@@ -107,6 +107,7 @@ export const CalculatorForm = ({ onCalculate, onClear, sugestoesProjetos = [] }:
           id="valorRolo"
           type="number"
           inputMode="decimal"
+          pattern="[0-9]*"
           placeholder="R$120.00"
           value={valorRolo}
           onChange={(e) => setValorRolo(e.target.value)}
@@ -121,6 +122,7 @@ export const CalculatorForm = ({ onCalculate, onClear, sugestoesProjetos = [] }:
             id="pesoRolo"
             type="number"
             inputMode="decimal"
+            pattern="[0-9]*"
             placeholder="1000g"
             value={pesoRolo}
             onChange={(e) => setPesoRolo(e.target.value)}
@@ -134,6 +136,7 @@ export const CalculatorForm = ({ onCalculate, onClear, sugestoesProjetos = [] }:
             id="pesoProjeto"
             type="number"
             inputMode="decimal"
+            pattern="[0-9]*"
             placeholder="50g"
             value={pesoProjeto}
             onChange={(e) => setPesoProjeto(e.target.value)}
